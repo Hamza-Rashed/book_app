8596941967e10940ce7a32e4490affc5df32307e
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2020-11-02 12:40:18 +0200

    true

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 'use strict';
 
 const
-    express = require('express'),
-    app = express(),
-    PORT = process.env.PORT || 3000,
-    superagent = require('superagent');
+  express = require('express'),
+  app = express(),
+  PORT = process.env.PORT || 3000,
+  superagent = require('superagent');
 
-// -------------------------- 
+// --------------------------
 app.use(express.static('public'));
 app.use(express.static('views'));
 app.use(express.urlencoded({extended:true}));
@@ -20,53 +20,57 @@ app.get('/search/new',renderForm)
 
 // ---------------------------
 function welcome(req,res){
-    res.render('pages/index');
+  res.render('pages/index');
 }
 
 function search(req,res){
-    var bookSearch = req.body.bookSearch;
-    var searchType = req.body.searchType;
-    let urlBooks;
-    if (searchType == 'title'){
-        urlBooks = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}+intitle`;
-    }else{
-        urlBooks = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}+inauthor`;
-    }
+    console.log('here');
+  var bookSearch = req.body.bookSearch;
+  var searchType = req.body.searchType;
+  console.log(bookSearch);
+  console.log(searchType);
 
-superagent.get(urlBooks).then(data =>{
+  let urlBooks;
+  if (searchType == 'title'){
+    urlBooks = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}+intitle`;
+  }else{
+    urlBooks = `https://www.googleapis.com/books/v1/volumes?q=${bookSearch}+inauthor`;
+  }
+
+  superagent.get(urlBooks).then(data =>{
     let result = data.body.items.map(element =>{
-        console.log(element.volumeInfo)
-        return new Book(element)
+      console.log(element.volumeInfo)
+      return new Book(element)
     });
     res.render('pages/searches/show',{booksResult: result });
 
-});
+  });
 }
 
 function renderForm(req,res){
-    res.render('pages/searches/new.ejs')
+  res.render('pages/searches/new.ejs')
 }
 
 function Book(data) {
-    this.bookName = data.volumeInfo.title;
-    this.bookAuthor = findData(data.volumeInfo.authors , 'cant find author');
-    this.bookDesc = findData(data.volumeInfo.subtitle , 'cant find any description');
-    this.bookImage = findData(data.volumeInfo.imageLinks.smallThumbnail , 'https://i.imgur.com/J5LVHEL.jpg')
-};
+  this.bookName = data.volumeInfo.title;
+  this.bookAuthor = findData(data.volumeInfo.authors , 'cant find author');
+  this.bookDesc = findData(data.volumeInfo.subtitle , 'cant find any description');
+  this.bookImage = findData(data.volumeInfo.imageLinks.smallThumbnail , 'https://i.imgur.com/J5LVHEL.jpg')
+}
 
 // helper functions
 
 function findData(data , massege) {
-    if(data){
-        return data;
-    }else{
-        return massege
-    }
+  if(data){
+    return data;
+  }else{
+    return massege
+  }
 }
 
 app.use('*',(req,res)=>{
-    res.render('pages/error.ejs')
+  res.render('pages/error.ejs')
 });
 app.listen(PORT,() =>{
-console.log(`listening to port : ${PORT}`);
+  console.log(`listening to port : ${PORT}`);
 });
